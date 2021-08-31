870e78195d46aa357f2476de7b911b09da479515
Simone <simone.cascino1984@gmail.com>
2021-08-31 17:17:50 +0200

    Add the other cases

--- app/src/main/java/it/simonecascino/destinationbuilder/MainActivity.kt
+++ app/src/main/java/it/simonecascino/destinationbuilder/MainActivity.kt
@@ -1,18 +1,19 @@
 package it.simonecascino.destinationbuilder
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.*
 import it.simonecascino.destination.Destinations
 import it.simonecascino.destinationbuilder.annotation.Destination
 import it.simonecascino.destinationbuilder.ui.theme.DestinationBuilderTheme
@@ -33,7 +34,7 @@ class MainActivity : ComponentActivity() {
 
                 val currentDestination: Destinations = if(currentRoute != null)
                     Destinations.fromPath(currentRoute)
-                else Destinations.HomePage
+                else Destinations.FirstDestination
 
                 val title = URLDecoder.decode(
                     navBackStackEntry?.arguments?.getString(Destinations.ANDROID_TITLE) ?: currentDestination.title,
@@ -58,18 +59,81 @@ class MainActivity : ComponentActivity() {
                     }
                 ) {
 
-                    NavHost(navController = navController, startDestination = Destinations.HomePage.route()){
+                    NavHost(navController = navController, startDestination = Destinations.FirstDestination.route()){
 
-                        composable(Destinations.HomePage.route()){
-                            HomePage{
+                        composable(route = Destinations.FirstDestination.route()){
+
+                            FirstDestination {
                                 navController.navigate(
-                                    Destinations.NextDestination.buildPath(URLEncoder.encode("Dynamic title", "UTF-8"))
+                                    Destinations.SecondDestination.buildPath("Ciao", 2.toString())
                                 )
                             }
                         }
 
-                        composable(Destinations.NextDestination.route()){
-                            NextDestination()
+                        composable(
+                            route = Destinations.SecondDestination.route(),
+                            arguments = listOf(
+                                navArgument(Destinations.SecondDestination.KEY_param1){
+                                    type = NavType.StringType
+                                },
+                                navArgument(Destinations.SecondDestination.KEY_param2){
+                                    type = NavType.IntType
+                                }
+                            )
+                        ){
+
+                            val param1 = it.arguments?.getString(Destinations.SecondDestination.KEY_param1) ?: ""
+                            val param2 = it.arguments?.getInt(Destinations.SecondDestination.KEY_param2) ?: 0
+
+                            SecondDestination(param1, param2){
+                                navController.navigate(
+                                    Destinations.ThirdDestination.buildPath(URLEncoder.encode("Third destination", "UTF-8"))
+                                )
+                            }
+                        }
+
+                        composable(
+                            route = Destinations.ThirdDestination.route(),
+                            arguments = listOf(
+                                navArgument(Destinations.ANDROID_TITLE){
+                                    type = NavType.StringType
+                                }
+                            )
+                        ){
+
+                            ThirdDestination{
+                                navController.navigate(
+                                    Destinations.FourthDestination.buildPath("Query1")
+                                )
+                            }
+                        }
+
+                        composable(
+                            route = Destinations.FourthDestination.route(),
+                            arguments = listOf(
+                                navArgument(Destinations.FourthDestination.KEY_query1){
+                                    type = NavType.StringType
+                                    nullable = true
+                                },
+                                navArgument(Destinations.FourthDestination.KEY_query2){
+                                    type = NavType.StringType
+                                    nullable = true
+                                },
+                                navArgument(Destinations.FourthDestination.KEY_query3){
+                                    type = NavType.StringType
+                                    nullable = true
+                                    defaultValue = "queryDefault"
+                                }
+                            )
+
+                        ){
+
+                            val query1 = it.arguments?.getString(Destinations.FourthDestination.KEY_query1)
+                            val query2 = it.arguments?.getString(Destinations.FourthDestination.KEY_query2)
+                            val query3 = it.arguments?.getString(Destinations.FourthDestination.KEY_query3)
+
+                            FourthDestination(query1 = query1, query2 = query2, query3 = query3)
+
                         }
 
                     }
@@ -82,19 +146,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+@Destination
+fun FirstDestination(goToNext: () -> Unit){
+
+    DestinationLayout(text = "First destination") {
+        goToNext()
+    }
+
+}
+
 @Destination(
-    title = "Home"
+    paths = ["param1", "param2"]
 )
-fun HomePage(goToNext: () -> Unit){
+@Composable
+fun SecondDestination(param1: String, param2: Int, goToNext: () -> Unit){
 
-    Column {
-        Text(
-            text = "Start destination"
-        )
-
-        Button(onClick = { goToNext() }) {
-            Text(text= "go to next")
-        }
+    DestinationLayout(text = "First param is $param1, second param (Int) is $param2") {
+        goToNext()
     }
 
 }
@@ -103,9 +171,57 @@ fun HomePage(goToNext: () -> Unit){
     dynamicTitle = true
 )
 @Composable
-fun NextDestination(){
+fun ThirdDestination(goToNext: () -> Unit){
+
+    DestinationLayout(text = "Third destination") {
+        goToNext()
+    }
+
+}
+
+@Destination(
+    queryParams = ["query1", "query2", "query3"]
+)
+@Composable
+fun FourthDestination(query1: String?, query2: String?, query3: String?){
+
+    DestinationLayout(text = "First query is $query1, second query is $query2, third query is $query3") {
+
+    }
+
+}
+
+@Composable
+@Destination(
+    paths = ["param1", "param2"],
+    queryParams = ["query1", "query2", "query3"]
+)
+fun FifthDestination(){
+
+}
+
+@Destination(
+    title = "Title"
+)
+@Composable
+fun SixthDestination(){
+
+}
 
 
+@Composable
+fun DestinationLayout(text: String, goToNext: () -> Unit){
+
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        Text(text = text)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { goToNext() }) {
+            Text(text= "go to next")
+        }
+    }
 
 }
 
